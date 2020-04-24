be5369a3a1e8685a475306338ef3b2a9a04bf2a0
ExoMeyada <ExoMeyada@users.noreply.github.com>
2020-04-24 19:26:55 +0200

    Dynamic map 1

---
+++ RobotShooter/Assets/Scripts/Controllers/Enemies/GroundEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GroundEnemy : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform point;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = point.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
