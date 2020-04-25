5356e6486a061709f3b668e59a4bf44f6af19b1f
ExoMeyada <ExoMeyada@users.noreply.github.com>
2020-04-25 21:15:51 +0200

    GroundEnemy

--- RobotShooter/Assets/Scripts/Controllers/Enemies/GroundEnemy.cs
+++ RobotShooter/Assets/Scripts/Controllers/Enemies/GroundEnemy.cs
@@ -5,20 +5,120 @@ using UnityEngine.AI;
 
 public class GroundEnemy : MonoBehaviour
 {
+    public enum State { INITIAL, CHASE, ATTACK, HIT, DEATH }
+    public State currentState = State.INITIAL;
     NavMeshAgent agent;
-    public Transform point;
-
+    public Transform player;
+    [Header("Stats")]
+    public float health;
+    public float minDistAttack;
+    public float maxDistAttack;
+    public float healthLostByHit;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = point.position;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case State.INITIAL:
+                ChangeState(State.CHASE);
+                break;
+            case State.CHASE:
+                if (health <= 0)
+                {
+                    ChangeState(State.DEATH);
+                    break;
+                }
+                if (DistanceToTargetSquared(gameObject, player.gameObject) <= minDistAttack * minDistAttack)
+                {
+                    ChangeState(State.ATTACK);
+                    break;
+                }
+                break;
+            case State.ATTACK:
+                if (health <= 0)
+                {
+                    ChangeState(State.DEATH);
+                    break;
+                }
+                if (DistanceToTargetSquared(gameObject, player.gameObject) >= maxDistAttack * maxDistAttack)
+                {
+                    ChangeState(State.CHASE);
+                    break;
+                }
+                break;
+            case State.HIT:
+                break;
+            case State.DEATH:
+                Destroy(gameObject, 1f);
+                break;
+        }
+    }
+
+    void ChangeState(State newState)
+    {
+        switch (currentState)
+        {
+            case State.CHASE:
+                agent.isStopped = true;
+                break;
+            case State.ATTACK:                
+                break;
+            case State.HIT:
+                break;
+            case State.DEATH:
+                break;
+        }
+
+        switch (newState)
+        {
+            case State.CHASE:
+                Debug.Log("Chase");
+                InvokeRepeating("GoToTarget", 0, 1f);
+                
+                agent.isStopped = false;
+                break;
+            case State.ATTACK:
+                Debug.Log("Attack");
+                break;
+            case State.HIT:
+                health -= healthLostByHit;
+                break;
+            case State.DEATH:
+                break;
+        }
+
+        currentState = newState;
+    }
+
+    float DistanceToTarget(GameObject me, GameObject target)
+    {
+        return (target.transform.position - me.transform.position).magnitude;
+    }
+
+    float DistanceToTargetSquared(GameObject me, GameObject target)
+    {
+        return (target.transform.position - me.transform.position).sqrMagnitude;
+    }
+
+    void GoToTarget()
+    {
+        agent.destination = player.position;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "PlayerBullet")//tag de la bullet del player
+        {
+            ChangeState(State.HIT);
+        }
     }
 }
