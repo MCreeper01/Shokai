e1535bcc5097cd172ee3ab3a30d3ceb502ab813a
MCreeper00 <adriamir@gmail.com>
2020-04-25 08:19:51 +0200

    Rounds now alternate maps

---
+++ RobotShooter/Assets/Scripts/Objects/Map.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Map
+{
+    public int id;
+    public enum Difficulty { EASY, MEDIUM, HARD };
+    public Difficulty difficulty;
+    public float[] enemySpawnPercentage;
+    public GameObject map; //Remove in the future!!!
+}
