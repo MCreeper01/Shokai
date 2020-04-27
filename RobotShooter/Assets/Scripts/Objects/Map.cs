af4970912633c24ef28d327455b48e7ff5870f25
MCreeper00 <adriamir@gmail.com>
2020-04-27 21:03:38 +0200

    Enemy spawns half way
    I calculate the amount of enemies to spawn and the time between enemy spawns but enemies do not spawn yet.

--- RobotShooter/Assets/Scripts/Objects/Map.cs
+++ RobotShooter/Assets/Scripts/Objects/Map.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 [Serializable]
 public class Map
 {
+    private const int ENEMIES = 2;
+
     public int id;
     public enum Difficulty { EASY, MEDIUM, HARD };
     public Difficulty difficulty;
-    public float[] enemySpawnPercentage;
+    public int[] enemySpawnPercentage = new int[ENEMIES];
     public GameObject map; //Remove in the future!!!
+    public GameObject bake;
+
+    void OnValidate()
+    {
+        if (enemySpawnPercentage.Length != ENEMIES)
+        {
+            Debug.LogWarning("Enemy Spawn Percentage array length cannot be changed on the inspector, please ask MCreeper00 if you want more info :)");
+            Array.Resize(ref enemySpawnPercentage, ENEMIES);
+        }
+    }
 }
