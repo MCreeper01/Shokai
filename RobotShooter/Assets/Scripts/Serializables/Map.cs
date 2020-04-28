e9858c9e45b14f8bf95dfb282fed13afc05f26d4
MCreeper00 <adriamir@gmail.com>
2020-04-28 02:32:36 +0200

    Spawners Work but need to be improved

---
+++ RobotShooter/Assets/Scripts/Serializables/Map.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Map
+{
+    public int id;
+    public enum Difficulty { EASY, MEDIUM, HARD };
+    public Difficulty difficulty;
+    public int[] enemySpawnPercentage;
+    public GameObject mapPrefab; //Remove in the future!!!
+    public GameObject bake;
+}
