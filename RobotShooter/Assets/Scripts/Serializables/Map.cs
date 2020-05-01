12a3e042ad8ef5f0fe41fec16488463d17c7f50b
MCreeper00 <adriamir@gmail.com>
2020-05-01 05:26:50 +0200

    Round Controller finished

--- RobotShooter/Assets/Scripts/Serializables/Map.cs
+++ RobotShooter/Assets/Scripts/Serializables/Map.cs
@@ -7,7 +7,9 @@ public class Map
     public int id;
     public enum Difficulty { EASY, MEDIUM, HARD };
     public Difficulty difficulty;
+    [Tooltip("Each slot represents the spawn percentage of each type of enemy on this map. The order of the enemies is the same as the Enemy list.")]
     public int[] enemySpawnPercentage;
     public GameObject mapPrefab; //Remove in the future!!!
+    [Tooltip("Bake of this map navmesh.")]
     public GameObject bake;
 }
