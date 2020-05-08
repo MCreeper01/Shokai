318e8cf553ca6ee2d73ba78f28e40a6406fede5e
MCreeper00 <adriamir@gmail.com>
2020-05-08 18:32:47 +0200

    Sistema events + debuging
    He fet un sistema d'events i m'he passat 4 anys debugejant coses del meu script que en pol m'havia tocat sense avisar ja que no m'he pogut comunicar amb ell de cap de les maneres.

---
+++ RobotShooter/Assets/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.instance.onRoundChange += OnRoundChange;
+    }
+
+    void OnRoundChange()
+    {
+        //Feu aquí el que hagueu de fer.
+    }
+
+}
