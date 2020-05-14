6143138932c5e2e2a582f09eb733beb69c91cbee
ExoMeyada <ExoMeyada@users.noreply.github.com>
2020-05-15 01:06:47 +0200

    Pathfinding3D 2

---
+++ RobotShooter/Assets/Scripts/Pathfinding3D/ProvisionalManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProvisionalManager : MonoBehaviour
+{
+    public static ProvisionalManager _instance;
+
+
+    public VolumetricGraph currentGraph;
+    
+    public static ProvisionalManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<ProvisionalManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        currentGraph.CrearNodes();
+        currentGraph.CrearConnexions();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
