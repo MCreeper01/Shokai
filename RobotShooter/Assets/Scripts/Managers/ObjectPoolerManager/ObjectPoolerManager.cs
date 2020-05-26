9f5ef354c986bf36289a715c2b4842ee824499e5
MCreeper00 <adriamir@gmail.com>
2020-05-27 01:18:28 +0200

    ObjectPoolerManager Implemented

---
+++ RobotShooter/Assets/Scripts/Managers/ObjectPoolerManager/ObjectPoolerManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ObjectPoolerManager : AController
+{
+
+    [Header("AR Bullet")]
+    public GameObject ARBullet;
+    public Transform ARBulletParent;
+    public int ARBulletAmount;
+    [HideInInspector] public ObjectPooler ARBulletOP;
+
+    [Header("Air Enemy Bullet")]
+    public GameObject airEnemyBullet;
+    public Transform airEnemyBulletParent;
+    public int airEnemyBulletAmount;
+    [HideInInspector] public ObjectPooler airEnemyBulletOP;
+
+    [Header("Tank Enemy Bullet")]
+    public GameObject tankEnemyBullet;
+    public Transform tankEnemyBulletParent;
+    public int tankEnemyBulletAmount;
+    [HideInInspector] public ObjectPooler tankEnemyBulletOP;
+
+    [HideInInspector] public ObjectPooler[] enemiesOP;
+
+    public void StartGame()
+    {
+        ARBulletOP = new ObjectPooler(ARBulletAmount, ARBullet, ARBulletParent);
+        airEnemyBulletOP = new ObjectPooler(airEnemyBulletAmount, airEnemyBullet, airEnemyBulletParent);
+        tankEnemyBulletOP = new ObjectPooler(tankEnemyBulletAmount, tankEnemyBullet, tankEnemyBulletParent);
+
+        Enemy[] enemies = gc.roundController.enemies;
+        enemiesOP = new ObjectPooler[enemies.Length];
+        for (int i = 0; i < enemiesOP.Length; i++)
+        {
+            enemiesOP[i] = new ObjectPooler(enemies[i].enemyMaxAmount, enemies[i].enemyPrefab, enemies[i].enemyParent);
+        }
+    }
+
+}
---
+++ RobotShooter/Assets/Scripts/Managers/ObjectPoolerManager/PoolableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PoolableObject
+{
+    public string name;
+    public GameObject prefab;
+    public Transform parent;
+    public int amount;
+    [HideInInspector] public ObjectPooler objectPooler;
+}
