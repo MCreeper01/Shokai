abd809f390219abfc4269afa7066e3dcc4ec5f31
polBru <polBru@users.noreply.github.com>
2020-05-27 01:18:23 +0200

    Load Scene implemented

---
+++ RobotShooter/Assets/Scripts/Controllers/Other/LoadSceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject loadCamera;
+    [SerializeField] private Image progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync((int)SceneIndexes.GAME, LoadSceneMode.Additive);
+
+        while (gameLevel.progress < 1)
+        {
+            progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
+        StartCoroutine(SetUpAll());
+    }
+
+    IEnumerator SetUpAll()
+    {
+        yield return new WaitForSeconds(0.2f);
+        loadCamera.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
---
+++ RobotShooter/Assets/Scripts/Other/SceneIndexes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public enum SceneIndexes
+{
+    MAIN_MENU = 0,
+    LOAD_SCENE = 1,
+    GAME = 2
+}
