199a7730f997ba3dfdd6c8216700c4b8b651006b
MCreeper00 <adriamir@gmail.com>
2020-05-31 07:37:15 +0200

    Textura Cúpula

---
+++ RobotShooter/Assets/Scripts/Other/RotateTexture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTexture : MonoBehaviour
+{
+
+    public float speed;
+    Vector2 offset = Vector2.zero;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.y = (offset.y + speed * Time.deltaTime) % 1;
+        GetComponent<MeshRenderer>().material.mainTextureOffset = offset;
+    }
+}
